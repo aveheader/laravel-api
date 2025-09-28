2cb3b5d97c56143a3bf658bcfd55fd6b281f6f76
Vasily <12stulevka@gmail.com>
2025-09-28 23:01:52 +0300

    Fix welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,5 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     API для управления задачами (тестовое задание): http://localhost/api
-    Читай README.md
     </body>
 </html>
